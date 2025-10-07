02a23985928d328aae5bf36bacb3e3b71cfb3840
cuong <cuong@CUONGDZ2004>
2025-10-07 23:06:09 +0700

    document01

---
+++ src/main/java/com/project/t8/entity/Department.java
@@ -0,0 +1,53 @@
+package com.project.t8.entity;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "department")
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "department_id")
+    private Long departmentId;
+
+    @Column(name = "department_name")
+    private String departmentName;
+
+    @Column(name = "division")
+    private String division;
+
+    @Column(name = "description")
+    private String description;
+
+    public Long getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Long departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public String getDivision() {
+        return division;
+    }
+
+    public void setDivision(String division) {
+        this.division = division;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ src/main/java/com/project/t8/entity/Document.java
@@ -0,0 +1,83 @@
+package com.project.t8.entity;
+import jakarta.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "documents")
+public class Document {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "document_id")
+    private Long documentId;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "upload_date")
+    private Timestamp uploadDate;
+
+    @Column(name = "file_path")
+    private String filePath;
+
+    @Column(name = "uploaded_by_user")
+    private String uploadedByUser;
+
+    @Column(name = "department_id")
+    private Long departmentId;
+
+    public Long getDocumentId() {
+        return documentId;
+    }
+
+    public void setDocumentId(Long documentId) {
+        this.documentId = documentId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Timestamp getUploadDate() {
+        return uploadDate;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public String getUploadedByUser() {
+        return uploadedByUser;
+    }
+
+    public void setUploadedByUser(String uploadedByUser) {
+        this.uploadedByUser = uploadedByUser;
+    }
+
+    public Long getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Long departmentId) {
+        this.departmentId = departmentId;
+    }
+}
