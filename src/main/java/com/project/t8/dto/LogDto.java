69b66aaa07fdd3dbe339df5c0f9a5df7437d7106
Ngoc Thinh Ngoc <thinhbaonhai@gmail.com>
2025-10-08 00:27:52 +0700

    dto02

--- src/main/java/com/project/t8/dto/LogDto.java
+++ src/main/java/com/project/t8/dto/LogDto.java
@@ -1,10 +1,16 @@
 package com.project.t8.dto;
 
+import java.sql.Timestamp;
+
 public class LogDto {
     private String action;
     private String target;
     private String status;
     private String description;
+    private Long documentId;
+    private Timestamp createdAt;
+    private Timestamp completedAt;
+    private Long departmentId;
 
     public String getAction() {
         return action;
@@ -32,5 +38,33 @@ public class LogDto {
     }
     public void setTarget(String target) {
         this.target = target;
+    }
+
+    public Long getDepartmentId() {
+        return departmentId;
+    }
+    public void setDepartmentId(Long departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Timestamp getCompletedAt() {
+        return completedAt;
+    }
+    public void setCompletedAt(Timestamp completedAt) {
+        this.completedAt = completedAt;
+    }
+
+    public Long getDocumentId() {
+        return documentId;
+    }
+    public void setDocumentId(Long documentId) {
+        this.documentId = documentId;
     }
 }
