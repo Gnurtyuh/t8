a454b345ed23c39ec506f20c556cad612ada51b6
main <gnurtyuh@gmail.com>
2025-10-07 19:23:34 +0700

    Create(project): create project and sql

---
+++ src/test/java/com/project/t8/T8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.t8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
